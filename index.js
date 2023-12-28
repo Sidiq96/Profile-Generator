5f57cfb965da324ec2506c2b464a278fa647c50c
Sidiq Hersi <hersisidiq@gmail.com>
2023-12-28 17:13:36 +0000

    adds the array of questions for manager, engineer and intern

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Empty array for team
+let team = [];
+
+// array of questions for manager
+const managerQuestions = [
+  { type: "input", message: "What is your Team manager's name?", name: "name" },
+  { type: "input", message: "What's Team manager's ID number?", name: "ID" },
+  { type: "input", message: "What's Team manager's email?", name: "Email" },
+  {
+    type: "input",
+    message: "What's Team manager's office number?",
+    name: "Office",
+  },
+];
+// array of questions for engineer
+const engineerQuestions = [
+  { type: "input", message: "What is your Engineer's name?", name: "name" },
+  { type: "input", message: "What's Engineer's ID number?", name: "ID" },
+  { type: "input", message: "What's Engineer's email?", name: "Email" },
+  { type: "input", message: "What's Engineer's Github", name: "Github" },
+  // array of questions for intern
+];
+const internQuestions = [
+  { type: "input", message: "What is your Intern's name?", name: "name" },
+  { type: "input", message: "What's Intern's ID number?", name: "ID" },
+  { type: "input", message: "What's Intern's email?", name: "Email" },
+  { type: "input", message: "What's Intern's school?", name: "School" },
+];
